560526615e04c13b4356de86ceefb3daeca66b8e
diegobcuadro <51387939+diegobcuadro@users.noreply.github.com>
2021-03-21 13:01:51 -0300

    Update cnn.py
    limpiando un poco del código..

--- experiment/cnn.py
+++ experiment/cnn.py
@@ -21,56 +21,6 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-
-class MLPClassifier(nn.Module):
-    def __init__(self,
-                 pretrained_embeddings_path,
-                 token_to_index,
-                 n_labels,
-                 hidden_layers=[256, 128],
-                 dropout=0.3,
-                 vector_size=300,
-                 freeze_embedings=True):
-        super().__init__()
-        with gzip.open(token_to_index, "rt") as fh:
-            token_to_index = json.load(fh)
-        embeddings_matrix = torch.randn(len(token_to_index), vector_size)
-        embeddings_matrix[0] = torch.zeros(vector_size)
-        with gzip.open(pretrained_embeddings_path, "rt") as fh:
-            next(fh)
-            for line in fh:
-                word, vector = line.strip().split(None, 1)
-                if word in token_to_index:
-                    embeddings_matrix[token_to_index[word]] =\
-                        torch.FloatTensor([float(n) for n in vector.split()])
-        self.embeddings = nn.Embedding.from_pretrained(embeddings_matrix,
-                                                       freeze=freeze_embedings,
-                                                       padding_idx=0)
-        self.hidden_layers = [
-            nn.Linear(vector_size, hidden_layers[0])
-        ]
-        for input_size, output_size in zip(hidden_layers[:-1], hidden_layers[1:]):
-            self.hidden_layers.append(
-                nn.Linear(input_size, output_size)
-            )
-        self.dropout = dropout
-        self.hidden_layers = nn.ModuleList(self.hidden_layers)
-        self.output = nn.Linear(hidden_layers[-1], n_labels)
-        self.vector_size = vector_size
-
-    def forward(self, x):
-        x = self.embeddings(x)
-        x = torch.mean(x, dim=1)
-        for layer in self.hidden_layers:
-            x = F.relu(layer(x))
-            if self.dropout:
-                x = F.dropout(x, self.dropout)
-        x = self.output(x)
-        return x
-
-    
-    
-    
 class CNNClassifier(nn.Module):
     def __init__(self, 
                  pretrained_embeddings_path, 
